9907935756b0b3fd56460465298b74090f60c374
MaxScarlet <maxim.kulik99@gmail.com>
2024-01-30 19:20:56 +0200

    Changes to create in controller, swagger

--- src/controllers/crudApiController.ts
+++ src/controllers/crudApiController.ts
@@ -3,77 +3,71 @@ import { StatusCodes } from "http-status-codes";
 import { GenericApiController } from "./genericApiController";
 
 export class CrudApiController<T> extends GenericApiController {
-  constructor(private service: CrudApiService<T>) {
-    super();
-  }
+	constructor(private service: CrudApiService<T>) {
+		super();
+	}
 
-  async handleRequest(event: APIGatewayProxyEvent): Promise<any> {
-    try {
-      const { httpMethod, path, body } = event;
+	async handleRequest(event: APIGatewayProxyEvent): Promise<any> {
+		try {
+			const { httpMethod, path, body } = event;
 
-      switch (httpMethod) {
-        case "GET":
-          const id = event.pathParameters?.id;
-          const queryString = event.queryStringParameters;
-          let resp: any;
-          if (id) {
-            resp = await this.service.get(id);
-          } else {
-            resp = await this.service.get_all(queryString);
-            if (resp == null) {
-              return this.errorResponse(StatusCodes.BAD_REQUEST); // handle validation error
-            }
-          }
-          if (!resp) {
-            return this.errorResponse(StatusCodes.NOT_FOUND);
-          }
-          return this.successResponse(resp);
+			switch (httpMethod) {
+				case "GET":
+					const id = event.pathParameters?.id;
+					const queryString = event.queryStringParameters;
+					let resp: any;
+					if (id) {
+						resp = await this.service.get(id);
+					} else {
+						resp = await this.service.get_all(queryString);
+						if (resp == null) {
+							return this.errorResponse(StatusCodes.BAD_REQUEST); // handle validation error
+						}
+					}
+					if (!resp) {
+						return this.errorResponse(StatusCodes.NOT_FOUND);
+					}
+					return this.successResponse(resp);
 
-        case "POST":
-          const itemPost = JSON.parse(body!);
-          await this.service.create(itemPost);
-          return this.successResponse(null, StatusCodes.CREATED);
+				case "POST":
+					const itemPost = JSON.parse(body!);
+					const item = await this.service.create(itemPost);
+					return this.successResponse(item, StatusCodes.CREATED);
 
-        case "PUT":
-          const idPut = event.pathParameters?.id;
-          if (!idPut) {
-            return this.errorResponse(
-              StatusCodes.BAD_REQUEST,
-              "ID is required"
-            );
-          }
-          const updatedItem = JSON.parse(body!);
-          const itemPut = await this.service.update(idPut, updatedItem);
-          if (!itemPut) {
-            return this.errorResponse(StatusCodes.NOT_FOUND);
-          }
-          return this.successResponse(itemPut);
+				case "PUT":
+					const idPut = event.pathParameters?.id;
+					if (!idPut) {
+						return this.errorResponse(StatusCodes.BAD_REQUEST, "ID is required");
+					}
+					const updatedItem = JSON.parse(body!);
+					const itemPut = await this.service.update(idPut, updatedItem);
+					if (!itemPut) {
+						return this.errorResponse(StatusCodes.NOT_FOUND);
+					}
+					return this.successResponse(itemPut);
 
-        case "DELETE":
-          const idDelete = event.pathParameters?.id;
-          if (!idDelete) {
-            return this.errorResponse(
-              StatusCodes.BAD_REQUEST,
-              "ID is required"
-            );
-          }
-          await this.service.delete(idDelete);
-          return this.successResponse(StatusCodes.GONE);
+				case "DELETE":
+					const idDelete = event.pathParameters?.id;
+					if (!idDelete) {
+						return this.errorResponse(StatusCodes.BAD_REQUEST, "ID is required");
+					}
+					await this.service.delete(idDelete);
+					return this.successResponse(StatusCodes.GONE);
 
-        default:
-          return this.errorResponse(StatusCodes.METHOD_NOT_ALLOWED);
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      return this.errorResponse(StatusCodes.INTERNAL_SERVER_ERROR);
-    }
-  }
+				default:
+					return this.errorResponse(StatusCodes.METHOD_NOT_ALLOWED);
+			}
+		} catch (error) {
+			console.error("Error:", error);
+			return this.errorResponse(StatusCodes.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
 
 export interface CrudApiService<T> {
-  get(id: string): Promise<T | null>;
-  get_all(queryString: any): Promise<T[] | null>;
-  create(item: T): Promise<void>;
-  update(id: string, item: T): Promise<T | null>;
-  delete(id: string): Promise<void>;
+	get(id: string): Promise<T | null>;
+	get_all(queryString: any): Promise<T[] | null>;
+	create(item: T): Promise<T>;
+	update(id: string, item: T): Promise<T | null>;
+	delete(id: string): Promise<void>;
 }
--- src/services/podcastService.ts
+++ src/services/podcastService.ts
@@ -8,39 +8,40 @@ import { SearchParams } from "../models/SearchParams";
 // import { DynamoDbHelper } from '../helpers/dynamoDbHelper';
 
 export class PodcastService implements CrudApiService<Podcast> {
-  // private readonly dbHelper: MongoDbHelper<DistributorDoc> | DynamoDbHelper<Distributor>;
+	// private readonly dbHelper: MongoDbHelper<DistributorDoc> | DynamoDbHelper<Distributor>;
 
-  constructor(private dbHelper: IDbHelper<PodcastDoc> | IDbHelper<Podcast>) {
-    // this.dbHelper = new MongoDbHelper<DistributorDoc>('Distributor', DistributorSchema, tableName);
-    // this.dbHelper = new DynamoDbHelper<Distributor>(tableName);
-  }
+	constructor(private dbHelper: IDbHelper<PodcastDoc> | IDbHelper<Podcast>) {
+		// this.dbHelper = new MongoDbHelper<DistributorDoc>('Distributor', DistributorSchema, tableName);
+		// this.dbHelper = new DynamoDbHelper<Distributor>(tableName);
+	}
 
-  async get_all(queryString: SearchParams): Promise<Podcast[] | null> {
-    // check mandatory field - PodcastID
-    if (queryString && !queryString.UserID) {
-      return null;
-    }
-    const fields = ["Title", "Description"];
+	async get_all(queryString: SearchParams): Promise<Podcast[] | null> {
+		// check mandatory field - PodcastID
+		if (queryString && !queryString.UserID) {
+			return null;
+		}
+		const fields = ["Title", "Description"];
 
-    const items = await this.dbHelper.get_list<Podcast>(queryString, fields);
-    return items;
-  }
+		const items = await this.dbHelper.get_list<Podcast>(queryString, fields);
+		return items;
+	}
 
-  async get(id: string): Promise<Podcast | null> {
-    const item = await this.dbHelper.get<Podcast>(id);
-    return item || null;
-  }
+	async get(id: string): Promise<Podcast | null> {
+		const item = await this.dbHelper.get<Podcast>(id);
+		return item || null;
+	}
 
-  async create(item: Podcast): Promise<void> {
-    await this.dbHelper.create<Podcast>(item);
-  }
+	async create(item: Podcast): Promise<Podcast> {
+		const response = await this.dbHelper.create<Podcast>(item);
+		return response;
+	}
 
-  async update(id: string, updated: Podcast): Promise<Podcast | null> {
-    const item = await this.dbHelper.update<Podcast>(id, updated);
-    return item || null;
-  }
+	async update(id: string, updated: Podcast): Promise<Podcast | null> {
+		const item = await this.dbHelper.update<Podcast>(id, updated);
+		return item || null;
+	}
 
-  async delete(id: string): Promise<void> {
-    await this.dbHelper.delete<Podcast>(id);
-  }
+	async delete(id: string): Promise<void> {
+		await this.dbHelper.delete<Podcast>(id);
+	}
 }
